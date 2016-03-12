a14212033650949c3d9bd417ca1118d1f30dfc90
Felipe Aníbal Torrejón Traslaviña <felipetorrejon@live.cl>
2016-03-11 23:17:18 -0300

    traspaso de codigo js a radio.js

--- radio.js
+++ radio.js
@@ -1,6 +1,16 @@
-var year,month,day,hour,minute,second,fecha;
-function fecha(year,month,day,hour,minute,second){
-	var fecha = new Date()
+var fecha = new Date();
+		function fechar(){
 
-}
-alert(year)
+		var numero,url;
+
+		fecha.setFullYear(document.getElementById("year").value);
+		fecha.setMonth(document.getElementById("month").value);
+		fecha.setDate(document.getElementById("day").value);
+		fecha.setHours(document.getElementById("hour").value);
+		fecha.setMinutes(document.getElementById("minute").value);
+		fecha.setSeconds(document.getElementById("second").value);
+		numero = Math.floor(parseInt(fecha.getTime()) / 1000);
+		url = "http://radioalacarta.cooperativa.cl/#" + numero;
+		document.getElementById("link").innerHTML = "<a href=\"" + url + "\"> " + url + "</a>";
+		}
+		document.getElementById("boton").addEventListener("click",fechar);
