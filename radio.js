566286c1d58de620a8c6b44991b116d09e8490a0
Felipe Aníbal Torrejón Traslaviña <felipetorrejon@live.cl>
2016-04-14 19:08:12 -0300

    Update radio.js
    reformatting file

--- radio.js
+++ radio.js
@@ -1,16 +1,17 @@
 var fecha = new Date();
-		function fechar(){
 
-		var numero,url;
+function fechar(){
+	var numero,url;
+	
+	fecha.setFullYear(document.getElementById("year").value);
+	fecha.setMonth(document.getElementById("month").value);
+	fecha.setDate(document.getElementById("day").value);
+	fecha.setHours(document.getElementById("hour").value);
+	fecha.setMinutes(document.getElementById("minute").value);
+	fecha.setSeconds(document.getElementById("second").value);
+	numero = Math.floor(parseInt(fecha.getTime()) / 1000);
+	url = "http://radioalacarta.cooperativa.cl/#" + numero;
+	document.getElementById("link").innerHTML = "<a href=\"" + url + "\"> " + url + "</a>";
+}
 
-		fecha.setFullYear(document.getElementById("year").value);
-		fecha.setMonth(document.getElementById("month").value);
-		fecha.setDate(document.getElementById("day").value);
-		fecha.setHours(document.getElementById("hour").value);
-		fecha.setMinutes(document.getElementById("minute").value);
-		fecha.setSeconds(document.getElementById("second").value);
-		numero = Math.floor(parseInt(fecha.getTime()) / 1000);
-		url = "http://radioalacarta.cooperativa.cl/#" + numero;
-		document.getElementById("link").innerHTML = "<a href=\"" + url + "\"> " + url + "</a>";
-		}
-		document.getElementById("boton").addEventListener("click",fechar);
+document.getElementById("boton").addEventListener("click",fechar);
